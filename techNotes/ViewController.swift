2c826e7ac591fd8d0fc00ac0e6c7e69766f76190
lionel_t <ltarlet@desktop-4jaki2s.viennedoc.com>
2016-05-09 15:47:32 +0200

    Initial Commit

---
+++ techNotes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  techNotes
+//
+//  Created by lionel_t on 09/05/2016.
+//  Copyright © 2016 VienneDoc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
